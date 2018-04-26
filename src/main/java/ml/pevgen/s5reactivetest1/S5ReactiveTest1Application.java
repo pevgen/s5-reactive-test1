0728ac94db54da516c567d3dc53d34bbf1934295
pevgen <pevgen@gmail.com>
2018-04-26 17:06:45 +0300

    added imitation of blocking response source

--- src/main/java/ml/pevgen/s5reactivetest1/S5ReactiveTest1Application.java
+++ src/main/java/ml/pevgen/s5reactivetest1/S5ReactiveTest1Application.java
@@ -1,7 +1,6 @@
 package ml.pevgen.s5reactivetest1;
 
 import org.reactivestreams.Publisher;
-import org.reactivestreams.Subscriber;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,20 +12,24 @@ import org.springframework.integration.handler.LoggingHandler;
 import org.springframework.integration.jms.dsl.Jms;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.messaging.Message;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.Disposable;
-import reactor.core.publisher.BaseSubscriber;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 import javax.jms.ConnectionFactory;
 import java.time.Duration;
-import java.util.List;
-import java.util.concurrent.TimeoutException;
+import java.time.LocalDateTime;
+import java.util.Random;
+import java.util.concurrent.SynchronousQueue;
 
 @SpringBootApplication
 @RestController
+@EnableScheduling
 public class S5ReactiveTest1Application {
 
     public static void main(String[] args) {
@@ -79,17 +82,48 @@ public class S5ReactiveTest1Application {
 //                    .timeout(Duration.ofSeconds(2), Mono.error(new RuntimeException("forced " + "failure")))
 //                    //.doAfterTerminate(() -> System.out.println("Do anything with your string" + "sdfg"))
 //                    .map(Message::getPayload)
-            mm = Mono.from(jmsReactiveSource())
-                    .timeout(Duration.ofSeconds(2))
-                    .map(Message::getPayload)
-                    .timeout(Duration.ofSeconds(2), Mono.just("timeout"))
+        mm = Mono.from(jmsReactiveSource())
+                .map(Message::getPayload)
+                .timeout(Duration.ofSeconds(10), Mono.just("timeout"))
 //                    .timeout(Duration.ofSeconds(2), Mono.error(new TimeoutException("Timeout after " + Duration.ofSeconds(2))))
 //                    .doAfterTerminate(() -> System.out.println("Do anything with your string !!!!!"))
 
-            ;
+        ;
 
         System.out.println(mm);
         return mm;
     }
 
+    @GetMapping(value = "/rr2")
+    public Mono<String> requestResponseWithParams(@RequestParam("id") String id) {
+
+        long timeout = (long) new Random().nextInt(4);
+
+        return Mono.defer(() -> anotherSource(id))
+                .subscribeOn(Schedulers.elastic())  // in other thread
+                .timeout(Duration.ofSeconds(timeout), Mono.just("timeout"))  // timeout
+                ;
+//        return Mono.from(anotherSource(id))
+//                .timeout(Duration.ofSeconds(1), Mono.just("timeout"))  // timeout
+//                ;
+
+    }
+
+    SynchronousQueue<String> synchronousQueue = new SynchronousQueue();
+
+    public Mono<String> anotherSource(String id) {
+        String event = null;
+        try {
+            event = synchronousQueue.take();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return Mono.just("xmlId:" + id + " \n event:" + event);
+    }
+
+
+    @Scheduled(fixedDelay = 5000)
+    public void imitateGettingResponseMessageFromResponseQueue() {
+        synchronousQueue.offer("event-response:" + LocalDateTime.now());
+    }
 }
